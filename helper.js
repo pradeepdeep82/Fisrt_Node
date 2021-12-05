a728e3a1b7dae5c62ac33bb502823a8b687ffcb0
pradeepdeep82 <pradeepdeep82@gmail.com>
2021-12-05 23:32:20 +0530

    object id done

--- helper.js
+++ helper.js
@@ -1,4 +1,4 @@
-
+import {ObjectId} from 'mongodb';
  
  async function AddMovie(client, data) {
   return await client
@@ -26,19 +26,22 @@ async function createUser(client, data) {
   return await client
     .db("myFirstDatabase")
     .collection("movies")
-    .updateOne({ id: id }, { $set: data });
+    // .updateOne({ id: id }, { $set: data });
+    .updateOne({ _id: ObjectId(id) }, { $set: data });
 }
  async function deleteMovieById(client, id) {
   return await client
     .db("myFirstDatabase")
     .collection("movies")
-    .deleteOne({ id: id });
+    // .deleteOne({ id: id });
+    .deleteOne({ _id: ObjectId(id)});
 }
  async function getMoviesById(client, id) {
   return await client
     .db("myFirstDatabase")
     .collection("movies")
-    .findOne({ id: id });
+    // .findOne({ id: id });
+    .findOne({ _id: ObjectId(id) });
 }
 
 async function getUserByName(client, username) {
